d213b0bdf847114979b1bd313ec356e1ab067f50
Mark Matviyiv <mmatviyiv@gmail.com>
2018-07-04 22:48:57 +0300

    Implemented Stripe charging

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -9,15 +9,24 @@ class RentalsController < ApplicationController
   def create
     renter = User.find_by_email(params[:'guest-email_address'])
 
-    card = PaymentCard.create(
+    card = PaymentCard.new(
         card_number: params[:'guest-number'],
         expiration_year: params[:'guest-expiration_year'],
         expiration_month: params[:'guest-expiration_month'],
         security_code: params[:'guest-cvv'],
         user_id: renter.id
     )
-
     listing = Listing.find(params[:listing_id])
+
+    stripe = Stripe::Token.create(:card => {
+        :number => card.card_number,
+        :exp_month => card.expiration_month,
+        :exp_year => card.expiration_year,
+        :cvc => card.security_code
+    })
+    charge = Stripe::Charge.create(:amount => listing.price, :currency => 'USD', :source => stripe.id)
+
+    card.update(charge_id: charge.id)
     listing.rent(renter: renter, card_id: card.id)
 
     render nothing: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180704154022) do
+ActiveRecord::Schema.define(version: 20180704193005) do
 
   create_table "bank_accounts", force: :cascade do |t|
     t.string   "account_number"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20180704154022) do
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
     t.integer  "user_id"
+    t.string   "charge_id"
   end
 
   add_index "payment_cards", ["user_id"], name: "index_payment_cards_on_user_id"
